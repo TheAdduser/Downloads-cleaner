6438f4865e2cde6f5f9f23d55cd246184e462a15
Bartosz Wrona <EasyRider190@gmail.com>
2023-12-02 19:43:16 +0100

    create function base

--- src/script.py
+++ src/script.py
@@ -1,17 +1,18 @@
 import os
 
 
-source_dir = "C:/Pobrane/"
-dest_dir_programs = "E:/POBRANE/Programs/"
-dest_dir_videos = "E:/POBRANE/Videos/"
-dest_dir_archive = "E:/POBRANE/Archive/"
-dest_dir_documents = "E:/POBRANE/Documents/"
-dest_dir_pictures = "E:/POBRANE/Pictures/"
-dest_dir_audio = "E:/POBRANE/Audio/"
-dest_dir_torrents = "E:/POBRANE/Torrents/"
-dest_dir_code = "E:/POBRANE/Code/"
-dest_dir_spreadsheet = "E:/POBRANE/Spreadsheets"
-dest_dir_presentation = "E:/POBRANE/Presentations"
+source_dir = 'C:\\Users\\Bartek\\Downloads'
+dest_dir_programs = 'E:\\POBRANE\\Programs'
+dest_dir_videos = "E:\\POBRANE\\Videos"
+dest_dir_archive = "E:\\POBRANE\\Archive\\"
+dest_dir_documents = "E:\\POBRANE\\Documents"
+dest_dir_pictures = "E:\\POBRANE\\Pictures"
+dest_dir_audio = "E:\\POBRANE\\Audio"
+dest_dir_torrents = "E:\\POBRANE\\Torrents"
+dest_dir_code = "E:\\POBRANE\\Code"
+dest_dir_spreadsheet = "E:\\POBRANE\\Spreadsheets"
+dest_dir_presentation = "E:\\POBRANE\\Presentations"
+
 
 programs_format = [".exe",".msi"]
 videos_format = [".mp4",".mov",".avi"]
@@ -23,3 +24,18 @@ torrent_format = [".iso", ".torrent"]
 code_format = [".html",".css",".js",".php",".tsx",".py",".sql",".cpp",".c",".java"]
 spreadsheet_format = [".xlsx"]
 presentation_format = [".pptx"]
+
+
+def move_programs():
+    for files in source_dir:
+        for file in files:
+            if file.endswith('.png'):
+                print('File found')
+
+
+def main():
+    move_programs()
+
+
+if __name__ == '__main__':
+    main()
