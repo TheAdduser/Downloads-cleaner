af325797e91763a54200b746f4223a775aab72ab
Bartosz Wrona <EasyRider190@gmail.com>
2023-12-02 18:45:54 +0100

    Add directory variables

---
+++ src/script.py
@@ -0,0 +1,12 @@
+import os
+
+
+source_dir = "C:/Pobrane"
+dest_dir_programs = "E:/POBRANE/Programs/"
+dest_dir_videos = "E:/POBRANE/Videos/"
+dest_dir_archive = "E:/POBRANE/Archive/"
+dest_dir_documents = "E:/POBRANE/Documents/"
+dest_dir_pictures = "E:/POBRANE/Pictures/"
+dest_dir_sounds = "E:/POBRANE/Sounds/"
+dest_dir_torrents = "E:/POBRANE/Torrents/"
+dest_dir_code = "E:/POBRANE/Code/"
